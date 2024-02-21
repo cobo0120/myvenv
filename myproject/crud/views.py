31527358bc8195ac8b7a6e8652e04161920be6d3
cobo0120 <cobo94251@gmail.com>
2024-02-21 10:43:13 +0900

    20240221

---
+++ myproject/crud/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Product
+
+
+class TopView(TemplateView):
+    template_name = "top.html"
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 3
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name_suffix = '_update_form'
